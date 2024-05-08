528f208d36458fc02ef6a40ee806e89c2004d044
Savitar_AI <141311341+Savitarn1@users.noreply.github.com>
2024-05-08 17:24:11 +0500

    Portfolio

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const sun = document.querySelector('.fa-sun');
 const moon = document.querySelector('.fa-moon');
+const sun1 = document.querySelector('.sun');
+const moon1 = document.querySelector('.moon');
 const c1 = document.querySelector('.c1');
+const c2 = document.querySelector('.c2');
 const header = document.querySelector('#header');
 const day = document.querySelector('.day');
+const day1 = document.querySelector('.day1');
 const user = document.querySelector(".user")
 const ring = document.querySelector('.ring')
 const element = document.querySelector('.backwhite');
@@ -30,6 +34,31 @@ day.addEventListener('click' , () => {
         sun.classList.remove('noim');
         moon.classList.add('noim');
         c1.classList.remove('tx');
+        header.classList.remove('bgc');
+        h1.classList.remove('cyan');
+        p.classList.remove('cyan');
+        link.classList.remove('cyan1');
+    }
+
+});
+day1.addEventListener('click' , () => {
+    if (style.backgroundColor === 'rgb(224, 224, 224)') {
+        sun1.classList.add('noim');
+        moon1.classList.remove('noim');
+        c2.classList.add('tx');
+        header.classList.add('bgc');
+        user.classList.add("user1");
+        ring.classList.add('brn');
+        h1.classList.add('cyan');
+        p.classList.add('cyan');
+        link.classList.add('cyan1');
+
+    } else {
+        ring.classList.remove('brn');
+        user.classList.remove("user1");
+        sun1.classList.remove('noim');
+        moon1.classList.add('noim');
+        c2.classList.remove('tx');
         header.classList.remove('bgc');
         h1.classList.remove('cyan');
         p.classList.remove('cyan');
