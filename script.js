4a5e80a458eaf18c21a4e06724798dbecafd64b1
Savitar_AI <141311341+Savitarn1@users.noreply.github.com>
2025-02-27 14:11:47 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,47 @@
 let menuIcon = document.querySelector('#menu-icon'),
-    navbar = document.querySelector('.navbar'),
-    sections = document.querySelectorAll('section'),
-    navLinks = document.querySelectorAll("header nav a")
+  navbar = document.querySelector('.navbar'),
+  sections = document.querySelectorAll('section'),
+  navLinks = document.querySelectorAll('header nav a');
 
+const colors = ['aqua', 'tulip', 'blood', 'lime', 'dodger', 'gold'];
+let number = Math.floor(Math.random() * 10) % colors.length;
+let body = document.querySelector('body');
+let checker = document.querySelector('.color');
+let checkedcolor = colors[number];
+body.className = checkedcolor;
+checker.innerHTML = `<option value="${checkedcolor}">${checkedcolor.toUpperCase()}</option>`;
 
-randomTheme()
+colors.forEach((color) =>
+  color != checkedcolor
+    ? (checker.innerHTML += `<option value="${color}">${color.toUpperCase()}</option>`)
+    : ''
+);
 
-function randomTheme() {
-  const colors = ['aqua' , 'tulip' , 'blood' , 'lime' , 'dodger' , 'gold']
-  let number = Math.floor(Math.random() * 10 ) % colors.length
-  let body = document.querySelector('body')
-  let color = colors[number]
-  body.className = `${color}`
-  
-}
-window.addEventListener('onscroll' , ()=>{
-  sections.forEach(e => {
-    let top = window.scrollY
-    let offset = e.offsetTop - 150
-    let height = e.offsetHeight
-    let id = e.getAttribute('id')
+window.addEventListener('onscroll', () => {
+  sections.forEach((e) => {
+    let top = window.scrollY;
+    let offset = e.offsetTop - 150;
+    let height = e.offsetHeight;
+    let id = e.getAttribute('id');
 
-    if(top >= offset && top < offset + height){
-      navLinks.forEach(links => {
-        links.classList.remove('active')
-        document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
-      })
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove('active');
+        document
+          .querySelector('header nav a[href*=' + id + ']')
+          .classList.add('active');
+      });
     }
-  })
-})
+  });
+});
 
-menuIcon.addEventListener('click' , () => {
-  menuIcon.classList.toggle('bx-x')
-  navbar.classList.toggle('active')
-})
+menuIcon.addEventListener('click', () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+});
 
-function selectColor(){
-  let select = document.querySelector('.color').value
-  let body = document.querySelector('body')
-  body.className = `${select}`
+function selectColor() {
+  let select = document.querySelector('.color').value;
+  let body = document.querySelector('body');
+  body.className = `${select}`;
 }
